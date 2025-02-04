273986dc3cd525c03810a6c62ccc8cc0151f368c
weiziqi <db176539@163.com>
2025-02-04 15:15:24 +0800

    test

--- spring-context/src/main/java/org/springframework/context/annotation/Import.java
+++ spring-context/src/main/java/org/springframework/context/annotation/Import.java
@@ -56,6 +56,10 @@ import java.lang.annotation.Target;
 public @interface Import {
 
 	/**
+	 * 1)直接写class,利用无参构造器创建出对象放在容器中
+	 * 2）ImportSelector
+	 * 3）ImportBeanDefinitionRegistrar
+	 *
 	 * {@link Configuration @Configuration}, {@link ImportSelector},
 	 * {@link ImportBeanDefinitionRegistrar}, or regular component classes to import.
 	 */
